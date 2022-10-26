f6b92e5a233d4a1dc83f1ef05d09fa2a32163a38
VbifVbif <97863225+VbifVbif@users.noreply.github.com>
2022-10-26 14:30:33 +0600

    Add files via upload

---
+++ src/project.py
@@ -0,0 +1,109 @@
+import collections
+from time import sleep
+from json2html import * 
+import requests, psycopg2, json
+from flask import Flask, render_template, request
+
+# Connection to DB
+conn = psycopg2.connect(database="nft_test", user = "postgres", password = "PASSWORD", host = "127.0.0.1", port = "5432")
+
+url = "https://solana-gateway.moralis.io/nft/mainnet/{}/metadata"
+
+headers = {
+    "accept": "application/json",
+    "X-API-Key": "YOUR OWN API-KEY"
+}
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        address = request.form.get('address')
+
+        # Checking if the nft table exists in our database
+        cur = conn.cursor()
+        cur.execute("select * from information_schema.tables where table_name='nft_table'")
+        check1 = bool(cur.rowcount)
+        check2 = False
+        # Checking if our table has this particular address
+        if check1:
+            cur.execute("SELECT mint FROM nft_table WHERE mint = %s", (address,))
+            check2 = (cur.fetchone() is not None)   
+
+        # If this address is already in our table, let's try to display it
+        if check2:
+
+            # The code below converts our record from the database to JSON, similar to what we get from Moralis API
+            cur.execute('''SELECT * FROM nft_table LEFT JOIN multiplex_table USING(metaplex_id) 
+                LEFT JOIN owners ON nft_table.mint = owners.nft_address WHERE mint=%s''', (address,))
+            rows = cur.fetchall()
+
+            owners = []
+            for owner in rows:
+                o = collections.OrderedDict()
+                o["address"] = owner[11]
+                o["share"] = owner[14]
+                o["verified"] = owner[13]
+                owners.append(o)
+
+            m = collections.OrderedDict()
+            m["isMutable"] = rows[0][9]
+            m["masterEdition"] = rows[0][10]
+            m["metadatauri"] = rows[0][5]
+            m["owners"] = owners
+            m["primarySaleHappened"] = rows[0][8]
+            m["sellerFeeBasisPoints"] = rows[0][7]
+            m["updateauthority"] = rows[0][6]
+            
+            n = collections.OrderedDict()
+            n["mint"] = rows[0][1]
+            n["name"] = rows[0][2]
+            n["standard"] = rows[0][3]
+            n["symbol"] = rows[0][4]
+            n['metaplex'] = m
+
+            j = json.dumps(n)
+            return render_template('result.html', table = json2html.convert(json = j))
+        # If nft address is not in our table, try to get it through Moralis API
+        else:
+            response = requests.get(url.format(address), headers=headers).json()
+            sleep(3)
+
+            # In case of Bad Request
+            if 'statusCode' in response:
+                return '''<h1>NFT with this address not found</h1>'''
+
+            # If the Bad Request did not occur, Moralis will return JSON with information about the NFT 
+            # The code below saves this information to the DB
+            with conn.cursor() as cur:
+                cur.execute(""" CREATE TABLE IF NOT EXISTS owners(
+                    owner_address text, nft_address text, verified integer, share integer, PRIMARY KEY (owner_address, nft_address)) """)
+                query_sql = """ INSERT INTO owners(owner_address, nft_address, verified, share) VALUES(%s, %s, %s, %s) """
+                for o in response['metaplex']['owners']:
+                    cur.execute(query_sql, (o['address'], response['mint'], o['verified'], o['share'],))
+                conn.commit()
+
+                cur.execute(""" CREATE TABLE IF NOT EXISTS multiplex_table(
+                    metaplex_id serial, metadataUri text, updateAuthority text, sellerFeeBasisPoints integer, primarySaleHappened integer, 
+                    isMutable boolean, masterEdition boolean, PRIMARY KEY (metaplex_id)) """)
+                query_sql = """ INSERT INTO multiplex_table(metadataUri, updateAuthority, sellerFeeBasisPoints, primarySaleHappened, 
+                    isMutable, masterEdition) VALUES(%s, %s, %s, %s, %s, %s) """
+                cur.execute(query_sql, (response['metaplex']['metadataUri'], response['metaplex']['updateAuthority'], 
+                    response['metaplex']['sellerFeeBasisPoints'], response['metaplex']['primarySaleHappened'],
+                    response['metaplex']['isMutable'], response['metaplex']['masterEdition'],))
+                conn.commit() 
+
+                cur.execute(""" CREATE TABLE IF NOT EXISTS nft_table(
+                    mint text, name text, standard text, symbol text, metaplex_id serial, PRIMARY KEY (mint),
+                    FOREIGN KEY (metaplex_id) REFERENCES multiplex_table(metaplex_id)) """)
+                query_sql = """ INSERT INTO nft_table(mint, name, standard, symbol) VALUES(%s, %s, %s, %s) """ 
+                cur.execute(query_sql, (response['mint'], response['name'], response['standard'], response['symbol'],))
+                conn.commit()
+            
+            return render_template('result.html', table = json2html.convert(json = response))
+
+    return render_template('index.html')  
+
+if __name__ == '__main__':
+    app.run(debug=True)
